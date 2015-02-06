affe57a50abf70fbecdfc0c7d10500df59cba5f6
root <root@localhost>
2015-02-06 08:31:17 -0500

    -

--- Item.cpp
+++ Item.cpp
@@ -2,14 +2,29 @@
 
 Item::Item() {}
 
-Item::Item(std::string name, bool stackable, int uses) {
-	this->name = name;uses
+Item::Item(int itemId, std::string name, int quantity, int uses){
+	this->itemId = itemId;
+	this->name = name;
+	this->quantity = quantity;
+	this->stackable = false;
+	this->effects = new Effects(uses);
+}
+
+Item::Item(int itemId, std::string name, int quantity, int uses, bool stackable){
+	this->itemId = itemId;
+	this->name = name;
+	this->quantity = quantity;
 	this->stackable = stackable;
 	this->effects = new Effects(uses);
-
 }
 
 bool Item::isConsumable() {
 	return this->effects->consumable;
+}
+
+//depreciated; loadItem(itemId)
+Item* Item::clone(){
+	return new Item(this->itemId, this->name,
+		this->quantity, this->effects->uses,this->stackable);
 }
 
