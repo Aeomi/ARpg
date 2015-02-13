f0246d54fca37c7354b555f94521a142154beea5
Aeomi <Aeomi@users.noreply.github.com>
2015-02-13 09:18:25 -0500

    Updated String Conversion to templatic

--- Convert.cpp
+++ Convert.cpp
@@ -1,7 +1,7 @@
 #include "Convert.h"
 
-//template <class T>
-std::string ALib::Convert::toString(int value){
+template <class T>
+std::string ALib::Convert::toString(T value){
 	std::ostringstream stream;
 	return (dynamic_cast<std::ostringstream&>(stream << std::dec << value)).str();
 }
